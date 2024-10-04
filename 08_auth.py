1b14ac3865d1747a8f5fb30376678d7ebcb540fe
Mdzaidsiddique <45.mdzaid@gmail.com>
2024-10-04 19:35:16 +0530

    update 08_auth.py

--- 08_auth.py
+++ 08_auth.py
@@ -1,8 +1,10 @@
 import requests
 
+# with wrong username and password
 # res = requests.get("https://the-internet.herokuapp.com/basic_auth", auth=("abcd", "abc"))
 
 # print(res.status_code) #status code 401
 
+# with correct username and password
 res = requests.get("https://the-internet.herokuapp.com/basic_auth", auth=("admin", "admin"))
 print(res.status_code) #status code 200
